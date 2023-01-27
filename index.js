7ad95526a2dc9227d4bb1f7a453d02c107dbb1a1
Hector17vaz <lx16987@innovaccion.mx>
2023-01-27 15:32:43 -0600

    cort

--- index.js
+++ index.js
@@ -24,28 +24,58 @@ fEncryptr = (stringencrypt) => {
     });
     return stringencrypt;
 }
+//Limpiar Pantalla
 clearPage=() => {
-    document.getElementById("text-encrypt").value = '';
-    document.getElementById("message").value = '';
-    document.getElementById("text-screen1").style.display = "block";
+    location.reload();
 }
+//Encriptar texto
 btnEncrypt = () => {
     let textoEncriptado = fEncrypt(document.getElementById("text-encrypt").value);
     document.getElementById("message").value = textoEncriptado;
-    document.getElementById("text-encrypt").placeholder = "Ingresa el texto aquí...";
-    document.getElementById("text-screen1").style.display = "none";
-    document.getElementById("result").style.display = "block";
+    document.getElementById("text-encrypt").placeholder = "Ingresa el texto que desees encriptar o desencriptar...";
+    document.getElementById("text-screen1").classList.add("animate__animated");
+    document.getElementById("text-screen1").classList.add("animate__bounceOut");
+    setTimeout(() => {
+        document.getElementById("text-screen1").classList.add("hidden");
+    }, 600);
+    setTimeout(()=>{
+        document.getElementById("result").style.display = "block";
+    }, 700)
+    document.getElementById("encryptShower").style.display = "block";
+    document.getElementById("decryptShower").style.display = "none";
+    document.getElementById("reload-button").style.display = "block";
 }
+//Desencriptar texto
 btnDecrypt = () => {
     let textoDesencriptado = fEncryptr(document.getElementById("text-encrypt").value);
     document.getElementById("message").value = textoDesencriptado;
-    document.getElementById("text-encrypt").placeholder = "Ingresa el texto aquí...";
-    document.getElementById("text-screen1").style.display = "none";
-    document.getElementById("result").style.display = "block";
+    document.getElementById("text-encrypt").placeholder = "Ingresa el texto que desees encriptar o desencriptar...";
+    document.getElementById("text-screen1").classList.add("animate__animated");
+    document.getElementById("text-screen1").classList.add("animate__bounceOut");
+    setTimeout(() => {
+        document.getElementById("text-screen1").classList.add("hidden");
+    }, 600);
+    setTimeout(()=>{
+        document.getElementById("result").style.display = "block";
+    }, 700);
+    document.getElementById("encryptShower").style.display = "none";
+    document.getElementById("decryptShower").style.display = "block";
+    document.getElementById("reload-button").style.display = "block";
 }
+//Copiar texto
 copyToClipBoard = () => {
     const content = document.getElementById("message");
     content.select();
     document.execCommand('copy');
     alert("¡Copiado!");
 }
+//Revober clases 
+setTimeout(function(){
+    document.querySelector("h1").classList.remove("animate__animated", "animate__bounceIn");
+}, 2000);
+setTimeout(function(){
+    document.querySelector("main").classList.remove("animate__animated", "animate__fadeIn");
+}, 2000);
+setTimeout(function(){
+    document.getElementById("text-screen1").classList.remove("animate__animated", "animate__bounceOut")
+}, 2000);
